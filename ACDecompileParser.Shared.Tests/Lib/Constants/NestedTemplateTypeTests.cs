30dec116e9d9e9ef1f3db64e9a8f22837152ed39
trevis <trevis@haxit.org>
2026-01-09 03:40:13 -0700

    Create NestedTemplateTypeTests.cs: Add test for nested types within template instantiations

---
+++ ACDecompileParser.Shared.Tests/Lib/Constants/NestedTemplateTypeTests.cs
@@ -0,0 +1,50 @@
+using ACDecompileParser.Shared.Lib.Constants;
+using Xunit;
+
+namespace ACDecompileParser.Shared.Tests.Lib.Constants;
+
+/// <summary>
+/// Tests for nested types within template instantiations.
+/// </summary>
+public class NestedTemplateTypeTests
+{
+    [Theory]
+    [InlineData("QTIsaac<8,unsigned long>::randctx", "ACBindings.QTIsaac__uint.randctx")]
+    [InlineData("DArray<int>::iterator", "ACBindings.DArray__int.iterator")]
+    [InlineData("Map<string,int>::Node", "ACBindings.Map__string__int.Node")]
+    [InlineData("Outer<T>::Inner::Deep", "ACBindings.Outer__T.Inner.Deep")]
+    public void MapType_NestedTypeWithinTemplateInstantiation_PreservesNestedTypeSuffix(string cppType, string expectedCsType)
+    {
+        // Act
+        var result = PrimitiveTypeMappings.MapType(cppType);
+
+        // Assert
+        Assert.Equal(expectedCsType, result);
+    }
+
+    [Fact]
+    public void MapType_TemplateWithoutNestedType_ReturnsJustTemplateType()
+    {
+        // Arrange
+        const string cppType = "QTIsaac<8,unsigned long>";
+
+        // Act
+        var result = PrimitiveTypeMappings.MapType(cppType);
+
+        // Assert
+        Assert.Equal("ACBindings.QTIsaac__uint", result);
+    }
+
+    [Fact]
+    public void MapType_NestedTypePointer_PreservesNestedTypeSuffix()
+    {
+        // Arrange
+        const string cppType = "QTIsaac<8,unsigned long>::randctx*";
+
+        // Act
+        var result = PrimitiveTypeMappings.MapType(cppType);
+
+        // Assert
+        Assert.Equal("ACBindings.QTIsaac__uint.randctx*", result);
+    }
+}
